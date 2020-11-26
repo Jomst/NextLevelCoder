d9e99300b84fb260b6c1d2e2c8cd9998efc1d171
Jomst <74927086+Jomst@users.noreply.github.com>
2020-11-26 10:27:51 -0400

    Clase 2
    Clase 2

---
+++ components/game.py
@@ -0,0 +1,36 @@
+import pygame
+
+from utils.constants import (
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+    TITLE
+)
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("titulo")
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    def run(self):
+        self.create_components()
+        self.playing = True
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        pygame.quit()
+
+    def create_components(self):
+        pass
+
+    def update(self):
+        pass
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+
+    def draw(self):
+        pass
