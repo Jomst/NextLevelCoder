a4ff8855b20652947f76fd63f34737f200b14035
Jomst <74927086+Jomst@users.noreply.github.com>
2020-12-01 20:14:38 -0400

    Clase 6
    Clase 6

--- components/game.py
+++ components/game.py
@@ -10,10 +10,11 @@ from utils.constants import (
     BLACK
 )
 
+
 class Game:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption("titulo")
+        pygame.display.set_caption(TITLE)
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
 
@@ -21,29 +22,31 @@ class Game:
         self.create_components()
         self.playing = True
         while self.playing:
-            self.clock.tick(60)
-            self.events()
-            self.update()
-            self.draw()
+             self.clock.tick(60)
+             self.events()
+             self.update()
+             self.draw()
         pygame.quit()
 
     def create_components(self):
         self.all_sprites = pygame.sprite.Group()
-        player = Player()
-        self.all_sprites.add(player)
+        self.player = Player(self)
+        self.all_sprites.add(self.player)
 
-        ball = pygame.sprite.Group()
+        balls = pygame.sprite.Group()
         ball = Ball()
         self.all_sprites.add(ball)
-
 
     def update(self):
         self.all_sprites.update()
 
     def events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.playing = False
+         for event in pygame.event.get():
+             if event.type == pygame.QUIT:
+                 self.playing = False
+             elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.player.shoot()
 
     def draw(self):
         self.screen.fill(BLACK)
--- components/player.py
+++ components/player.py
@@ -5,10 +5,14 @@ from utils.constants import (
     SCREEN_HEIGHT,
     SCREEN_WIDTH
 )
+
+from components.bullet import Bullet
+
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, game):
         pygame.sprite.Sprite.__init__(self)
+        self.game = game
         self.image = pygame.Surface((50,50))
         self.image.fill(WHITE)
         self.rect = self.image.get_rect()
@@ -16,13 +20,20 @@ class Player(pygame.sprite.Sprite):
         self.rect.bottom = SCREEN_HEIGHT - 10
 
     def update(self):
+        self.movement_on_x = 10
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
-            self.rect.x += -5
+            self.rect.x = self.rect.x - self.movement_on_x
         elif key[pygame.K_RIGHT]:
-            self.rect.x += 5
+            self.rect.x = self.rect.x + self.movement_on_x
 
         if self.rect.left <= 0:
             self.rect.left = 0
         elif self.rect.right >= SCREEN_WIDTH:
             self.rect.right = SCREEN_WIDTH
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        self.game.all_sprites.add(bullet)
+        self.bullets = pygame.sprite.Group()
+        self.bullets.add(bullet)
